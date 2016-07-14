76d21e5bbd17a2093345261fc10c08b6f371cf12
iceStone <ice@micua.com>
2016-07-14 17:06:33 +0800

    initial commit

--- app/backgrounds/index.js
+++ app/backgrounds/index.js
@@ -1,54 +1,32 @@
-import './background'
+// This is main process of Electron, started as first thing when your
+// app starts. This script is running through entire life of your application.
+// It doesn't have any windows which you can see on screen, but we can open
+// window from here.
 
-// import { app, BrowserWindow } from 'electron'
+import { app } from 'electron'
+import setAppMenu from './menu'
+import createWindow from './window'
 
-// // Keep a global reference of the window object, if you don't, the window will
-// // be closed automatically when the JavaScript object is garbage collected.
-// let mainWindow
+let mainWindow
 
-// function createWindow () {
-//   // Create the browser window.
-//   mainWindow = new BrowserWindow({ width: 800, height: 600 })
+app.on('ready', () => {
+  setAppMenu()
+  mainWindow = createWindow('main', {
+    width: 1700,
+    height: 600,
+    useContentSize: true
+    // frame: false,
+    // backgroundColor: 'transparent',
+    // transparent: true
+  })
+  if (process.env.NODE_ENV === 'production') {
+    mainWindow.loadURL(`file://${__dirname}/index.html`)
+  } else {
+    mainWindow.loadURL('http://localhost:8080/index.html')
+    mainWindow.webContents.openDevTools({ detach: false })
+  }
+})
 
-//   // and load the index.html of the app.
-//   // Open the DevTools.
-//   if (process.env.NODE_ENV === 'production') {
-//     mainWindow.loadURL(`file://${__dirname}/index.html`)
-//   } else {
-//     mainWindow.loadURL(`http://localhost:8080/index.html`)
-//     mainWindow.webContents.openDevTools({ detach: true })
-//   }
-
-//   // Emitted when the window is closed.
-//   mainWindow.on('closed', () => {
-//     // Dereference the window object, usually you would store windows
-//     // in an array if your app supports multi windows, this is the time
-//     // when you should delete the corresponding element.
-//     mainWindow = null
-//   })
-// }
-
-// // This method will be called when Electron has finished
-// // initialization and is ready to create browser windows.
-// // Some APIs can only be used after this event occurs.
-// app.on('ready', createWindow)
-
-// // Quit when all windows are closed.
-// app.on('window-all-closed', () => {
-//   // On OS X it is common for applications and their menu bar
-//   // to stay active until the user quits explicitly with Cmd + Q
-//   if (process.platform !== 'darwin') {
-//     app.quit()
-//   }
-// })
-
-// app.on('activate', () => {
-//   // On OS X it's common to re-create a window in the app when the
-//   // dock icon is clicked and there are no other windows open.
-//   if (mainWindow === null) {
-//     createWindow()
-//   }
-// })
-
-// // In this file you can include the rest of your app's specific main process
-// // code. You can also put them in separate files and require them here.
+app.on('window-all-closed', () => {
+  app.quit()
+})
